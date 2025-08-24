8fc383412a98e94d84a0757dd52a91caf6be1eed
dinuprof04 <52514404+dinuprof04@users.noreply.github.com>
2025-08-24 11:44:29 -0400

    functions assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -2,12 +2,9 @@
 
 // A function that assigns two variables, concatenates them, and writes to the page
 function showMessage() {
-  // Assign two variables
-  const part1 = "Project 2: ";
-  const part2 = "Functions and DOM access";
 
-  // Concatenate the variables
-  const message = part1 + part2;
+  var message = "Project 2: "
+  message += "Functions and DOM access";
 
   // Get the paragraph by its id and set its text
   // The id must match the one used in index.html
